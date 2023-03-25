6a227c8ae867a77efe03dc606d69e7932f94dbd7
wangziyuan.12 <wangziyuan.12@bytedance.com>
2023-03-25 11:30:23 -0400

    bug fix

--- audio/transcribe_all.sh
+++ audio/transcribe_all.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 
 apt-get update && apt-get install git -y
-pip3 install -r requirements.txt
+pip3 install -r ../requirements.txt
 pip3 install "git+https://github.com/openai/whisper.git" 
 apt-get install -y ffmpeg
 python3 transcribe_all.py
