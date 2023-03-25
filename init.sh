8c4a7875cf5a1b0451d41ddec26aba26985b49d1
wangziyuan.12 <wangziyuan.12@bytedance.com>
2023-03-24 22:54:40 -0400

    run on native machine

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+apt-get update && apt-get install git -y
+pip3 install -r requirements.txt
+pip3 install "git+https://github.com/openai/whisper.git" 
+apt-get install -y ffmpeg
+python3 -m flask run --host=0.0.0.0
